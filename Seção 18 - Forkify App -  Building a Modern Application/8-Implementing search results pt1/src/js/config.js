301146a3705345c541f6c18b79295e39b11e9c0c
LucasPerrotaRoriz <Lucaswebdeveloper@proton.me>
2024-03-11 20:33:43 -0300

    Implementing search results

---
+++ Seção 18 - Forkify App -  Building a Modern Application/8-Implementing search results pt1/src/js/config.js
@@ -0,0 +1,7 @@
+// For constant variables and that should be used. 
+// Store variables that are important data for the app
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+
+
---
+++ Seção 18 - Forkify App -  Building a Modern Application/8-Implementing search results pt1/src/js/controller.js
@@ -0,0 +1,56 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+
+if (import.meta.hot) {
+  import.meta.hot.accept((newModule) => {
+    if (newModule) {
+      // newModule is undefined when SyntaxError happened
+      console.log('updated: count is now ', newModule.count)
+    }
+  })
+}
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1) Loading recipe
+    await model.loadRecipe(id);
+
+    // 2) Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    console.log('inside controller');
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function() {
+  try {
+    resultsView.renderSpinner();
+
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if(!query) return 
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) Render results
+    resultsView.render(model.state.search.results);
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+const init = function() {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+
+}
+init();
